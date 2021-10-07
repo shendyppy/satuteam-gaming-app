10382cc10137875a9a0294f2427ea9f295513439
Shendy <shendyppy@gmail.com>
2021-10-07 15:34:51 +0700

    init provider, router, pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./stores";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+
+import Add from "./pages/Add";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Edit from "./pages/Edit";
+import Detail from "./pages/Detail";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Provider store={store}>
+				<Navbar />
+				<Switch>
+					<Route path="/detail/:id">
+						<Detail />
+					</Route>
+					<Route path="/edit/:id">
+						<Edit />
+					</Route>
+					<Route path="/add">
+						<Add />
+					</Route>
+					<Route path="/cart">
+						<Cart />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</Provider>
+		</Router>
+	);
 }
 
 export default App;
