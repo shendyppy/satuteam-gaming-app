a13586f77076a263575f03d34dfd2880deee608e
Shendy <shendyppy@gmail.com>
2021-10-09 01:50:47 +0700

    deploying server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+const router = require("./routers");
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
+
+app.use(router);
 
 app.listen(PORT, () => {
 	console.log(`Listening to the PORT: ${PORT}`);
